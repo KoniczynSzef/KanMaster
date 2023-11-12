bd806bad50fe84f12a88b90f7ae8967ba3d22c01
Koniczyn <piotr12konczyk27@gmail.com>
2023-11-12 12:49:00 +0100

    Added checking for session in auth pages

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,11 +1,20 @@
 'use client';
 
+import { options } from '@/auth/options';
 import ForgotPassword from '@/components/Form/ForgotPassword';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import React, { FC } from 'react';
 
 interface Props {}
 
-const page: FC<Props> = () => {
+const page: FC<Props> = async () => {
+    const session = await getServerSession(options);
+
+    if (session) {
+        redirect('/dashboard');
+    }
+
     return (
         <section className="mt-36 flex justify-center items-center">
             <ForgotPassword />
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,18 @@
+import { options } from '@/auth/options';
 import SignIn from '@/components/Form/SignIn';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import React, { FC } from 'react';
 
 interface Props {}
 
-const page: FC<Props> = () => {
+const page: FC<Props> = async () => {
+    const session = await getServerSession(options);
+
+    if (session) {
+        redirect('/dashboard');
+    }
+
     return (
         <section className="mt-36 flex justify-center items-center">
             <SignIn />
