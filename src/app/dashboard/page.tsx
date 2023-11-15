72937c62a54b27603eb3fbdbe6a0be7b798af188
Koniczyn <piotr12konczyk27@gmail.com>
2023-11-15 21:30:23 +0100

    Features | Added projext-functions.ts file and changed schema.prisma a little

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { options } from '@/auth/options';
+import Test from '@/components/Test';
 import { getProjects } from '@/controllers/project-functions';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
@@ -14,11 +15,14 @@ const page: FC<Props> = async () => {
     }
 
     const projects = await getProjects(session?.user?.email);
+    console.log(projects);
 
     return (
         <div className="p-24">
             <h3 className="text-3xl font-bold">Dashboard Page</h3>
             <pre className="mt-6">{JSON.stringify(session.user)}</pre>
+
+            <Test email={session.user?.email} />
         </div>
     );
 };
--- src/controllers/project-functions.ts
+++ src/controllers/project-functions.ts
@@ -2,6 +2,7 @@
 
 import { db } from '@/db';
 import { getUser } from './user-functions';
+import { Project } from '@prisma/client';
 
 export async function getProjects(userEmail: string | null | undefined) {
     const user = await getUser(userEmail);
@@ -17,4 +18,24 @@ export async function getProjects(userEmail: string | null | undefined) {
     });
 
     return userProjects;
+}
+
+export async function createProject(
+    project: Omit<Project, 'id'>,
+    userEmail: string | null | undefined
+) {
+    const user = await getUser(userEmail);
+
+    if (!user) {
+        throw new Error('There is no user with that email');
+    }
+
+    const newProject = await db.project.create({
+        data: {
+            ...project,
+            teamLeaderId: user.id,
+        },
+    });
+
+    return newProject;
 }
