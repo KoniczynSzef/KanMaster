999d5b565e30e0ab069031052c553c266818b8ba
Koniczyn <piotr12konczyk27@gmail.com>
2023-12-03 21:48:18 +0100

    Structure | Separated Delete component from Project

--- src/app/dashboard/projects/[id]/page.tsx
+++ src/app/dashboard/projects/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { getProject } from '@/controllers/project-functions';
 import React, { FC } from 'react';
+import Delete from './Delete';
+import { getServerSession } from 'next-auth';
+import { options } from '@/auth/options';
+import { getUser } from '@/controllers/user-functions';
 
 interface Props {
     params: {
@@ -7,11 +11,31 @@ interface Props {
     };
 }
 
-const page: FC<Props> = async ({ params }) => {
+const Page: FC<Props> = async ({ params }) => {
     const { id } = params;
     const project = await getProject(id);
 
-    return <div>{project?.name}</div>;
+    const session = await getServerSession(options);
+
+    const user = await getUser(session?.user?.email);
+
+    if (!project) {
+        return <h3 className="font-bold text-2xl">Project not found!</h3>;
+    }
+
+    if (!user) {
+        return <h3 className="font-bold text-2xl">User not found!</h3>;
+    }
+
+    return (
+        <div className="container mx-auto py-24 flex justify-between gap-10">
+            <pre className="border border-muted rounded p-4">
+                {JSON.stringify(project, null, 2)}
+            </pre>
+
+            <Delete project={project} user={user} />
+        </div>
+    );
 };
 
-export default page;
+export default Page;
--- src/components/Dashboard/Projects/Project/Project.tsx
+++ src/components/Dashboard/Projects/Project/Project.tsx
@@ -3,11 +3,6 @@ import React, { FC } from 'react';
 import ProjectBadge from '../Badge';
 
 import { motion } from 'framer-motion';
-import { Button } from '@/components/ui/button';
-import { X } from 'lucide-react';
-import { deleteProject } from '@/controllers/project-functions';
-import { toast } from 'sonner';
-import { useRouter } from 'next/navigation';
 import { User } from '@prisma/client';
 
 import { Badge } from '@/components/ui/badge';
@@ -20,24 +15,9 @@ interface Props {
     isLeader: boolean;
 }
 
-const Project: FC<Props> = ({ project, idx, user, isLeader }) => {
-    const router = useRouter();
-    const { badges, setProjects, projects } = useProjectStore();
+const Project: FC<Props> = ({ project, idx, isLeader }) => {
+    const { badges } = useProjectStore();
     const badge = badges.find((badge) => badge.projectId === project.id);
-
-    const handleDelete = async () => {
-        try {
-            toast.info('Deleting project...');
-            await deleteProject(project.id ?? '', user?.email);
-
-            setProjects(projects.filter((p) => p.id !== project.id));
-
-            toast.success('Project deleted!');
-            return router.push('/dashboard');
-        } catch (error) {
-            toast.error('Something went wrong while deleting the project!');
-        }
-    };
 
     return (
         <Link
@@ -50,15 +30,6 @@ const Project: FC<Props> = ({ project, idx, user, isLeader }) => {
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.15 * idx }}
             >
-                <Button
-                    variant={'destructive'}
-                    size={'icon'}
-                    className="absolute right-0 top-0"
-                    onClick={handleDelete}
-                >
-                    <X />
-                </Button>
-
                 <ProjectBadge withId badge={badge} withoutAnimation />
                 <div className="flex-col">
                     <h3 className="text-3xl font-bold">{project.name}</h3>
